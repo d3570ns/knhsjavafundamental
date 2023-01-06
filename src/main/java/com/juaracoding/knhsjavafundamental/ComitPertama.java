ba77a9a4c23885a127b4f03831839e712583d4d4
d3570ns <nodestonk94@gmail.com>
2023-01-06 22:29:48 +0700

    Dalam perbaikan

--- src/main/java/com/juaracoding/knhsjavafundamental/ComitPertama.java
+++ src/main/java/com/juaracoding/knhsjavafundamental/ComitPertama.java
@@ -1,4 +1,10 @@
 package com.juaracoding.knhsjavafundamental;
 
 public class ComitPertama {
+
+
+    public static void main(String args[])
+    {
+        System.out.println("HELLO WORLD !!");
+    }
 }
