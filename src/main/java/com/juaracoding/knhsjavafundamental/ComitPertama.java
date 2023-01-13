5bd285a3ac868735e08f6c1530ae4b64f3423c69
d3570ns <nodestonk94@gmail.com>
2023-01-13 18:59:20 +0700

    Dalam perbaikan

--- src/main/java/com/juaracoding/knhsjavafundamental/ComitPertama.java
+++ src/main/java/com/juaracoding/knhsjavafundamental/ComitPertama.java
@@ -8,3 +8,4 @@ public class ComitPertama {
         System.out.println("HELLO WORLD !!");
     }
 }
+
---
+++ src/main/java/com/juaracoding/knhsjavafundamental/testarray/TestArray.java
@@ -0,0 +1,14 @@
+package com.juaracoding.knhsjavafundamental.testarray;
+/*
+Created By IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author User a.k.a. Kevin Nodeston Hasudungan Silalahi
+Java Developer
+Created on 13/01/2023 18:40
+@Last Modified 13/01/2023 18:40
+Version 1.0
+*/
+
+public class TestArray {
+}
+
