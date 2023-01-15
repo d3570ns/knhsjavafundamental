19fd6164387b5de15835c16c9105f3a0bd7c7963
d3570ns <nodestonk94@gmail.com>
2023-01-15 23:13:08 +0700

    Dalam perbaikan

---
+++ src/main/java/com/juaracoding/knhsjavafundamental/testharian/praktikum_1/Praktikum1.java
@@ -0,0 +1,40 @@
+package com.juaracoding.knhsjavafundamental.testharian.praktikum_1;
+/*
+Created By IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author User a.k.a. Kevin Nodeston Hasudungan Silalahi
+Java Developer
+Created on 15/01/2023 22:37
+@Last Modified 15/01/2023 22:37
+Version 1.0
+*/
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Praktikum1 {
+    public static void main(String[] args) {
+
+       char chX = 'U';
+       int intX = chX;
+        System.out.println(intX);
+        Scanner scanner = new Scanner(System.in);
+        String strx = scanner.nextLine();
+        System.out.println("Nilai STRX Adalah:"+strx);
+
+        int intTampung = 0;
+
+        for (int i=0;i<strx.length();i++){
+            chX = strx.charAt(i);
+            intX = chX;
+            if (intX>=32 && intX<=90){
+                intTampung = intTampung + intX;
+                System.out.println("INDEX KE "+i+" HURUF "+chX+" KODE ASCII "+intX);
+
+            }
+        }
+        System.out.println("NILAI INT TAMPUNG ADALAH :"+intTampung);
+
+            }
+        }
+
